bbda8cc382b1d196e313047da2820f06b33fd7f9
kajal sahani <166373141+kajalbk@users.noreply.github.com>
2024-04-08 14:38:31 +0530

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,13 @@
+function event1() {
+    let first = document.getElementById('fc').value;
+    document.getElementById('div1').style.color = first;
+}
+function event2() {
+    let second = document.getElementById('fs').value;
+    document.getElementById('div1').style.fontSize = second;
+
+}
+function event3() {
+    let third = document.getElementById('bg').value;
+    document.getElementById('div1').style.backgroundColor = third;
+}
